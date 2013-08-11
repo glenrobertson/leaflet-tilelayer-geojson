ca98f26c13a7bf723e61f52fbb732584702b34c1
Glen Robertson <robertson.glen@gmail.com>
2013-08-11 15:48:18 +1200

    Added feature grouping and layer clipping functionality.
    * Features are now grouped into GeometryCollections, grouped by the
    reuslt of the unique() function.
    * Added SVG clipping, so that features overlapping multiple tiles with
    a pixel overflow can have their outlines hidden.
    * Updated README: added contributor and added better documentation.

--- TileLayer.GeoJSON.js
+++ TileLayer.GeoJSON.js
@@ -18,7 +18,7 @@ L.TileLayer.Ajax = L.TileLayer.extend({
         this._loadTile(tile, tilePoint);
     },
     // XMLHttpRequest handler; closure over the XHR object, the layer, and the tile
-    _xhrHandler: function (req, layer, tile) {
+    _xhrHandler: function (req, layer, tile, tilePoint) {
         return function() {
             if (req.readyState != 4) {
                 return;
@@ -26,9 +26,9 @@ L.TileLayer.Ajax = L.TileLayer.extend({
             var s = req.status;
             if ((s >= 200 && s < 300) || s == 304) {
                 tile.datum = JSON.parse(req.responseText);
-                layer._tileLoaded();
+                layer._tileLoaded(tile, tilePoint);
             } else {
-                layer._tileLoaded();
+                layer._tileLoaded(tile, tilePoint);
             }
         }
     },
@@ -38,13 +38,13 @@ L.TileLayer.Ajax = L.TileLayer.extend({
         var layer = this;
         var req = new XMLHttpRequest();
         this._requests.push(req);
-        req.onreadystatechange = this._xhrHandler(req, layer, tile);
+        req.onreadystatechange = this._xhrHandler(req, layer, tile, tilePoint);
         req.open('GET', this.getTileUrl(tilePoint), true);
         req.send();
     },
-    _resetCallback: function() {
+    _reset: function() {
         this._data = [];
-        L.TileLayer.prototype._resetCallback.apply(this, arguments);
+        L.TileLayer.prototype._reset.apply(this, arguments);
         for (var i in this._requests) {
             this._requests[i].abort();
         }
@@ -57,57 +57,192 @@ L.TileLayer.Ajax = L.TileLayer.extend({
     }
 });
 
+
 L.TileLayer.GeoJSON = L.TileLayer.Ajax.extend({
-    _geojson: {"type":"FeatureCollection","features":[]},
+    _keyLayers: {},
+
+    // used to calculate svg path string for clip path elements
+    _clipPathRectangles: {},
+
     initialize: function (url, options, geojsonOptions) {
         L.TileLayer.Ajax.prototype.initialize.call(this, url, options);
-        this.geojsonLayer = new L.GeoJSON(this._geojson, geojsonOptions);
-        this.geojsonOptions = geojsonOptions;
+        this.geojsonLayer = new L.GeoJSON(null, geojsonOptions);
     },
     onAdd: function (map) {
         this._map = map;
         L.TileLayer.Ajax.prototype.onAdd.call(this, map);
-        this.on('load', this._tilesLoaded);
         map.addLayer(this.geojsonLayer);
     },
     onRemove: function (map) {
         map.removeLayer(this.geojsonLayer);
-        this.off('load', this._tilesLoaded);
         L.TileLayer.Ajax.prototype.onRemove.call(this, map);
     },
-    data: function () {
-        this._geojson.features = [];
-        if (this.options.unique) {
-            this._uniqueKeys = {};
-        }
-        var tileData = L.TileLayer.Ajax.prototype.data.call(this);
-        for (var t in tileData) {
-            var tileDatum = tileData[t];
-            if (tileDatum && tileDatum.features) {
+    _reset: function () {
+        this.geojsonLayer.clearLayers();
+        this._keyLayers = {};
+        this._removeOldClipPaths();
+        L.TileLayer.Ajax.prototype._reset.apply(this, arguments);
+    },
 
-                // deduplicate features by using the string result of the unique function
-                if (this.options.unique) {
-                    for (var f in tileDatum.features) {
-                        var featureKey = this.options.unique(tileDatum.features[f]);
-                        if (this._uniqueKeys.hasOwnProperty(featureKey)) {
-                            tileDatum.features.splice(f, 1);
-                        }
-                        else {
-                            this._uniqueKeys[featureKey] = featureKey;
-                        }
-                    }
+    // remove clip path elements from other earlier zoom levels
+    _removeOldClipPaths: function  () {
+        for (var clipPathId in this._clipPathRectangles) {
+            var clipPathZXY = clipPathId.split('_').slice(1);
+            if (parseInt(clipPathZXY[0]) !== this._map.getZoom()) {
+                var rectangle = this._clipPathRectangles[clipPathId];
+                this._map.removeLayer(rectangle);
+                var clipPath = document.getElementById(clipPathId);
+                if (clipPath !== null) {
+                    clipPath.parentNode.removeChild(clipPath);
                 }
-                this._geojson.features =
-                    this._geojson.features.concat(tileDatum.features);
+                delete this._clipPathRectangles[clipPathId];
             }
         }
-        return this._geojson;
     },
-    _resetCallback: function () {
-        this._geojson.features = [];
-        L.TileLayer.Ajax.prototype._resetCallback.apply(this, arguments);
+
+    // recurse sub-layers and call func() on L.Path layer instances
+    _recurseLayerUntilPath: function (func, layer) {
+        if (layer instanceof L.Path) {
+            func(layer);
+        }
+        else if (layer instanceof L.LayerGroup) {
+            // recurse each child layer
+            layer.getLayers().forEach(this._recurseLayerUntilPath.bind(this, func), this);
+        }
     },
-    _tilesLoaded: function (evt) {
-        this.geojsonLayer.clearLayers().addData(this.data());
+
+    _clipLayerToTileBoundary: function (layer, tilePoint) {
+        // Only use SVG clipping if the browser is using it
+        if (!L.Path.SVG) { return; }
+
+        var svg = this._map._pathRoot;
+
+        // create the defs container if it doesn't exist
+        var defs = null;
+        if (svg.getElementsByTagName('defs').length === 0) {
+            defs = document.createElementNS(L.Path.SVG_NS, 'defs');
+            svg.insertBefore(defs, svg.firstChild);
+        }
+        else {
+            defs = svg.getElementsByTagName('defs')[0];
+        }
+
+        // create the clipPath for the tile if it doesn't exist
+        var clipPathId = 'tileClipPath_' + tilePoint.z + '_' + tilePoint.x + '_' + tilePoint.y;
+        var clipPath = document.getElementById(clipPathId);
+        if (clipPath === null) {
+            clipPath = document.createElementNS(L.Path.SVG_NS, 'clipPath');
+            clipPath.id = clipPathId;
+
+            var tileSize = this.options.tileSize,
+            nwPoint = tilePoint.multiplyBy(tileSize),
+            sePoint = nwPoint.add([tileSize, tileSize]),
+            nw = this._map.unproject(nwPoint),
+            se = this._map.unproject(sePoint);
+
+            this._clipPathRectangles[clipPathId] = new L.Rectangle(new L.LatLngBounds([nw, se]), {
+                opacity: 0, 
+                fillOpacity: 0,
+                noClip: true
+            });
+            this._map.addLayer(this._clipPathRectangles[clipPathId]);
+
+            var path = document.createElementNS(L.Path.SVG_NS, 'path');
+            var pathString = this._clipPathRectangles[clipPathId].getPathString();
+            path.setAttribute('d', pathString);
+            clipPath.appendChild(path);
+            defs.appendChild(clipPath);
+        }
+
+        // add the clip-path attribute to reference the id of the tile clipPath
+        this._recurseLayerUntilPath(function (pathLayer) {
+            pathLayer._container.setAttribute('clip-path', 'url(#' + clipPathId + ')');
+        }, layer);
+        
+    },
+
+    // Copied from L.GeoJSON.addData
+    // Modified to:
+    // * merge geometries into Geometry Collections with the same this.options.unique() result
+    // * clip layers to tile boundaries (uses tilePoint parameter)
+    addTileData: function (geojson, tilePoint) {
+        var features = L.Util.isArray(geojson) ? geojson : geojson.features,
+            i, len, feature;
+
+        if (features) {
+            for (i = 0, len = features.length; i < len; i++) {
+                // Only add this if geometry or geometries are set and not null
+                feature = features[i];
+                if (feature.geometries || feature.geometry || feature.features || feature.coordinates) {
+                    this.addTileData(features[i], tilePoint);
+                }
+            }
+            return this;
+        }
+
+        var options = this.geojsonLayer.options;
+
+        if (options.filter && !options.filter(geojson)) { return; }
+
+        var incomingLayer = null;
+        if (this.options.unique && typeof(this.options.unique) === 'function') {
+            var key = this.options.unique(geojson);
+
+            // When creating the layer for a unique key
+            // Force the geojson to be a geometry collection
+            if (!(key in this._keyLayers && geojson.geometry.type !== 'GeometryCollection')) {
+                geojson.geometry = {
+                    type: 'GeometryCollection',
+                    geometries: [geojson.geometry]
+                };
+            }
+
+            // Transform the geojson into a new layer
+            try {
+                incomingLayer = L.GeoJSON.geometryToLayer(geojson, options.pointToLayer, options.coordsToLatLng);
+            }
+            catch (e) {
+                // could not parse geojson
+                return this;
+            }
+
+            // Add incoming layer to existing key's GeometryCollection
+            if (key in this._keyLayers) {
+                var existingLayer = this._keyLayers[key];
+                existingLayer.feature.geometry.geometries.push(geojson.geometry);
+                existingLayer.addLayer(incomingLayer);
+            }
+            // Convert the incoming geojson feature into a new GeometryCollection
+            else {
+                incomingLayer.feature = L.GeoJSON.asFeature(geojson);
+                this.geojsonLayer.addLayer(incomingLayer);
+                this._keyLayers[key] = incomingLayer;
+            }
+        }
+        // Add the incoming geojson feature to the GeoJSON layer
+        else {
+            incomingLayer = L.GeoJSON.geometryToLayer(geojson, options.pointToLayer, options.coordsToLatLng);
+            incomingLayer.feature = L.GeoJSON.asFeature(geojson);
+            this.geojsonLayer.addLayer(incomingLayer);
+        }
+
+        // if clipTiles is set, then clip the layer
+        if (this.options.clipTiles) {
+            this._clipLayerToTileBoundary(incomingLayer, tilePoint);
+        }
+
+        incomingLayer.defaultOptions = incomingLayer.options;
+        this.geojsonLayer.resetStyle(incomingLayer);
+
+        if (options.onEachFeature) {
+            options.onEachFeature(geojson, incomingLayer);
+        }
+        return this;
+    },
+
+    _tileLoaded: function (tile, tilePoint) {
+        L.TileLayer.Ajax.prototype._tileLoaded.apply(this, arguments);
+        if (tile.datum === null) { return null; }
+        this.addTileData(tile.datum, tilePoint);
     }
 });
